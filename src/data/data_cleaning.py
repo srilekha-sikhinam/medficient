af0733c28ed7e9f7d2ef973f0a10e5fe2222348c
Urvi Joshi <urvij9@gmail.com>
2022-12-06 21:52:05 -0500

    Updated file paths in cleaning files

--- src/data/data_cleaning.py
+++ src/data/data_cleaning.py
@@ -1,26 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 21 12:58:29 2022
-
-@author: Mitch.Burke
-"""
-
 import pandas as pd
 
 # First, we'll import our unioned dataset (runtime ~45 seconds)
-df = pd.read_csv('data\processed\Hospital_Inpatient_Discharges_17_18.csv')
-
-# Take all columns outside of the original index (first column)
-df = df.iloc[:, 1:]
-
+df = pd.read_csv('data/processed/Hospital_Inpatient_Discharges_17_18.csv')
 
 # Let's run some observational metrics on this dataset
-
 print('Overall Shape:')
 print(df.shape, '\n')
 
 print('Records per year:')
-print(df['year'].value_counts(), '\n')
+print(df['Discharge Year'].value_counts(), '\n')
 
 # Show the column names
 print('Columns: ')
@@ -49,7 +37,7 @@ df = df.loc[:, ~df.columns.isin(cols_to_omit)]
 # very small amount of records. 
 df = df[df['Length of Stay'] != '120 +']
 
-# Finally, let's take one last look at the data. We'll clean 
+# Finally, let's take one last look at the data. We'll clean it thereafter
 df_head = df.head()
 uniq_vals = df.apply(lambda col: col.unique())
 
@@ -58,7 +46,7 @@ print(df.dtypes, '\n')
 
 
 ##############################################################################
-# Using this information, we can format each field individually. We'll gon one
+# Using this information, we can format each field individually. We'll go one
 # by one and format those that need changing.
 
 # Proper case
@@ -79,18 +67,21 @@ edi_dict = {'N': 0, 'Y': 1, 'True': 1, 'False': 0, True: 1, False: 0}
 df['Emergency Department Indicator'] = df['Emergency Department Indicator'].map(edi_dict)
 
 # Finally, we convert certain data types in need of conversion
-df['Length of Stay'] = df['Length of Stay'].astype(int)
+df['Length of Stay'] = df['Length of Stay'].astype(float)
+
+
+# Drop columns that are not helpful
+df = df.loc[:, ~df.columns.isin(['Facility Name', 'Operating Certificate Number', 'Zip Code - 3 digits',
+        'Discharge Year', 'APR MDC Description'])]
 
 # As a last step, we'll reorder the columns so that our target variables are 
 # all the way to the right
-cols_fin = ['year', 'Hospital Service Area', 'Hospital County',
-            'Operating Certificate Number', 'Permanent Facility Id',
-            'Facility Name', 'Age Group', 'Zip Code - 3 digits', 
-            'Gender', 'Race', 'Ethnicity', 'Type of Admission',
-            'Patient Disposition', 'Discharge Year', 'CCSR Diagnosis Code',
+cols_fin = ['Hospital Service Area', 'Hospital County', 'Permanent Facility Id',
+            'Age Group', 'Gender', 'Race', 'Ethnicity', 
+            'Type of Admission', 'Patient Disposition', 'CCSR Diagnosis Code',
             'CCSR Diagnosis Description', 'CCSR Procedure Code',
             'CCSR Procedure Description', 'APR DRG Code', 'APR DRG Description',
-            'APR MDC Code', 'APR MDC Description', 'APR Severity of Illness Code',
+            'APR MDC Code', 'APR Severity of Illness Code',
             'APR Severity of Illness Description', 'APR Risk of Mortality',
             'APR Medical Surgical Description', 'Payment Typology 1',
             'Emergency Department Indicator', 'Total Costs', 'Total Charges',
@@ -101,8 +92,26 @@ df = df.loc[:, cols_fin]
 unique_vals_fin = df.apply(lambda col: col.unique())
 
 ###############################################################################
-# Now we can extract the finalized and cleaned dataset
-df.to_csv('data\processed\Hospital_Inpatient_Discharges_17_18_cleaned.csv')
+# We'll export datasets for five of the most pertinent illnesses found in 
+# the dataset, and all five combined
+illnesses = ['Heart Failure', 
+             'Chronic Obstructive Pulmonary Disease',
+             'Knee Joint Replacement',
+             'Kidney & Urinary Tract Infections',
+             'Schizophrenia']
 
+# Export all five datasets combined
+df_exp = df[df['APR DRG Description'].isin(illnesses)]
 
+df_exp.to_csv('data/processed/Hospital_Inpatient_Discharges_17_18_cleaned.csv', index = False)
 
+# Loop over them
+for i in illnesses:
+    
+    # Filter the df
+    df_temp = df[df['APR DRG Description'] == i]
+    
+    df_temp.to_csv('data/processed/HID_cleaned_' + i.replace(' ', '') + '.csv', index = False)
+    
+# Print export note
+print('Data Extracted')
