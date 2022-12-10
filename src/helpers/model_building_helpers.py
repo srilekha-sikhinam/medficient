31acf61b5482c2efce3a42dd6cd8f23e6b245a71
srilekha-sikhinam <82412858+srilekha-sikhinam@users.noreply.github.com>
2022-12-10 11:28:42 -0500

    Updating model helpers to include cost models

--- src/helpers/model_building_helpers.py
+++ src/helpers/model_building_helpers.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import _pickle as cpickle
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold, RandomizedSearchCV
 from sklearn.dummy import DummyClassifier
@@ -25,6 +25,21 @@ def get_train_test_data(df):
 
     return X, y, X_train, X_test, y_train, y_test
 
+def get_cost_train_test_data(df):
+    df = df.dropna()
+    X = df.loc[:, ~df.columns.isin(['Total Costs', 'Total Charges', 'Total Costs Bin','Length of Stay', 'Patient Disposition'])]
+    y = df[['Total Costs Bin']]
+
+    categorical_columns = list(df.select_dtypes(include='object'))
+
+    X = pd.get_dummies(X)
+
+    X = X.loc[:, ~X.columns.isin(categorical_columns)]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.30)
+
+    return X, y, X_train, X_test, y_train, y_test
+
 
 #Run randomizedsearch to get the best parameters for RandomForestClassifier
 def get_best_rf_params(X_train, y_train, param_grid, iterations):
@@ -38,6 +53,16 @@ def get_best_rf_params(X_train, y_train, param_grid, iterations):
     print(best_score)
     return best_params, best_score
 
+def get_best_gbc_params(X_train, y_train, param_grid, iterations):
+    cross_val = StratifiedKFold(n_splits=3)
+    clf = RandomizedSearchCV(HistGradientBoostingClassifier(), n_iter=iterations, param_distributions=param_grid, cv = cross_val, scoring='f1_macro', verbose=True, n_jobs=-1)
+
+    clf.fit(X_train, np.ravel(y_train))
+    # Fit on data
+    best_params = clf.best_params_
+    best_score = clf.best_score_
+    print(best_params, best_score)
+    return best_params, best_score
 
 #Run randomizedsearch to get the best parameters for LogisticRegression
 def get_best_lr_params(X_train, y_train, param_grid, iterations):
@@ -60,6 +85,15 @@ def train_model(best_params, X_train, y_train, model_file_name=None):
             cpickle.dump(model, file)
     return model
 
+def train_cost_model(best_params, X_train, y_train, model_file_name=None):
+    print("Training model")
+    model = HistGradientBoostingClassifier(max_depth = best_params['max_depth'], learning_rate = best_params['learning_rate'], min_samples_leaf = best_params['min_samples_leaf'], loss = best_params['loss'])
+    model.fit(X_train, np.ravel(y_train))
+    if model_file_name != None:
+        with open(model_file_name, 'wb') as file:  
+            cpickle.dump(model, file)
+    return model
+
 
 def calculate_scores(model, X_test, y_test):
     print("Getting model scores")
@@ -72,6 +106,11 @@ def calculate_scores(model, X_test, y_test):
 
 def get_model_scores(best_params, X_train, X_test, y_train, y_test):
     trained_model = train_model(best_params, X_train, y_train)
+    model_scores = calculate_scores(trained_model, X_test, y_test)
+    return model_scores
+
+def get_cost_model_scores(best_params, X_train, X_test, y_train, y_test):
+    trained_model = train_cost_model(best_params, X_train, y_train)
     model_scores = calculate_scores(trained_model, X_test, y_test)
     return model_scores
 
